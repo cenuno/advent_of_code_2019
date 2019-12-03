f1bc4faa3912bc53080b0d8a7873392d32e78c8e
cenuno <24255819+cenuno@users.noreply.github.com>
2019-12-02 23:25:26 -0800

    Finished part one of day 2

--- day_02/part_01_solution.py
+++ day_02/part_01_solution.py
@@ -70,3 +70,10 @@ assert calculate_intcode_program([1, 0, 0, 0, 99]) == [2, 0, 0, 0, 99]
 assert calculate_intcode_program([2, 3, 0, 3, 99]) == [2, 3, 0, 6, 99]
 assert calculate_intcode_program([2, 4, 4, 5, 99, 0]) == [2, 4, 4, 5, 99, 9801]
 assert calculate_intcode_program([1, 1, 1, 4, 99, 5, 6, 0, 99]) == [30, 1, 1, 4, 2, 5, 6, 0, 99]
+
+# use the calculator on the intcode program
+results = calculate_intcode_program(intcode_program)
+
+print(f"""
+The value left at position 0 after the program halts is {results[0]}
+""")
