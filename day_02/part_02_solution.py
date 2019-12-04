32a02027c1096d396ce1afbb0aeb3b8f6e164745
cenuno <24255819+cenuno@users.noreply.github.com>
2019-12-03 16:42:19 -0800

    Add solution to day 2 part 2

--- day_02/part_02_solution.py
+++ day_02/part_02_solution.py
@@ -4,27 +4,28 @@ what pair of inputs produces the output 19690720
 """
 from part_01_solution import calculate_intcode_program
 
-for noun in range(0, 100):
-    for verb in range(0, 100):
-        with open("data/day_02_input.txt", "r") as f:
-            intcode_program = [int(item) for item in f.read().split(",")]
-            # before running the program,
-            # replace position `1` with the value noun and
-            # replace position `2` with the value verb
-            intcode_program[1] = noun
-            intcode_program[2] = verb
-        # print(f"first 5 of IP: {intcode_program[0:5]}")
+# load necessary data
+with open("data/day_02_input.txt", "r") as f:
+    intcode_program = [int(item) for item in f.read().split(",")]
+
+# start the brute force inspection for the right pair
+for noun in range(100):
+    for verb in range(100):
+        # before running the program,
+        # replace position `1` with the value noun and
+        # replace position `2` with the value verb
+        intcode_program[1] = noun
+        intcode_program[2] = verb
         # use the calculator on the intcode program
         results = calculate_intcode_program(intcode_program)
-        # print(f"first 5 of results: {results[0:5]}")
-        # print(f"The value left at position 0 after the program halts is {results[0]}")
+        # check if position `0` contains the desired value
         if results[0] == 19690720:
+            print(f"results = {results[0]}")
+            print(f"noun = {noun}")
+            print(f"verb = {verb}")
+            print(f"""
+            The value of 100 * {noun} + {verb} is {100 * noun + verb}
+            """)
             break
         else:
             continue
-
-print(f"noun = {noun}")
-print(f"verb = {verb}")
-print(f"""
-The value of 100 * {noun} + {verb} is {100 * noun + verb}
-""")
