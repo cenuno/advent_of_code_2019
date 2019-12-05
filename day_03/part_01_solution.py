34c2ed81e5bfbff32e0db837b6077ff0ad45b585
cenuno <24255819+cenuno@users.noreply.github.com>
2019-12-04 22:20:06 -0800

    Add method to create coordinate pairs per wire

--- day_03/part_01_solution.py
+++ day_03/part_01_solution.py
@@ -15,11 +15,44 @@ CENTRAL_PORT_POS = (0, 0)
 
 
 def calculate_sign(direction: List[str]) -> List[Tuple[int, int]]:
-    """Determines the sign for each direction recorded along the wire
+    """Determines the sign for each step recorded along the wire's direction
     Args:
         - direction (List[str]): the direction recorded at a particular point
                                  along a wire
     Results:
         List[Tuple[int, int]]: List of each coordinate pair, stored as a tuple
     """
-    pass
+    # if 'R' or 'U' appears in the direction, ensure it is a pos int;
+    # otherwise make it negative
+    steps = [int(dir[1:])
+             if dir.find("R") == 0 or dir.find("U") == 0
+             else -int(dir[1:])
+             for dir in direction]
+
+    # store each odd numbered element as the x-coordinate
+    x_coords = steps[0::2]
+    # store each even numbered element as the y-coordinate
+    y_coords = steps[1::2]
+
+    if len(y_coords) == len(x_coords):
+        pass
+    elif len(y_coords) > len(x_coords):
+        # if there are more y-coords than x-coords
+        # pad the difference in x_coords with zeros
+        diff = len(y_coords) - len(x_coords)
+        x_coords.extend([0] * diff)
+    else:
+        # if there are more x-coords than y-coords
+        # pad the difference in y_coords with zeros
+        diff = len(x_coords) - len(y_coords)
+        y_coords.extend([0] * diff)
+
+    # create the coordinate pairs
+    coords = []
+    for x_coord, y_coord in zip(x_coords, y_coords):
+        coords.append((x_coord, y_coord))
+
+    return coords
+
+
+assert calculate_sign(["R1004", "U518", "R309", "D991"]) == [(1004, 518), (309, -991)]
