1543518338472dd26f8182314f986e8c57db3048
cenuno <24255819+cenuno@users.noreply.github.com>
2019-12-04 23:07:21 -0800

    Identify position of each point along the path of a wire

--- day_03/part_01_solution.py
+++ day_03/part_01_solution.py
@@ -5,16 +5,19 @@ use the Manhattan distance for this measurement.
 """
 from typing import List, Tuple
 
+# create new data type
+CoordPair = List[Tuple[int, int]]
+
 # load necessary data
 with open("data/day_03_input.txt", "r") as f:
     # in a list, store each wire's directional path as a list of strings
     wires = [wire.split(",") for wire in f.read().split("\n")]
 
 # store position of central port
-CENTRAL_PORT_POS = (0, 0)
+ORIGIN = (0, 0)
 
 
-def calculate_relative_coords(direction: List[str]) -> List[Tuple[int, int]]:
+def calculate_relative_coords(direction: List[str]) -> CoordPair:
     """Determines the relative coordinate pair for each step along a wire
     Note:
         - The key term here is 'relative'. Each coordinate pair is a step in
@@ -62,3 +65,32 @@ def calculate_relative_coords(direction: List[str]) -> List[Tuple[int, int]]:
 # check work
 assert calculate_relative_coords(["R1004", "U518", "R309", "D991"]) == [(1004, 518), (309, -991)]
 assert calculate_relative_coords(["L998", "U952", "R204", "U266"]) == [(-998, 952), (204, 266)]
+
+
+def calculate_position(relative_coords: CoordPair) -> CoordPair:
+    """Calculate the position of each coordinate pair from the central port
+    Args:
+        - relative_coords (CoordPair): the relative coordinate pair at a 
+                                       particular point along a wire
+    Returns:
+        CoordPair: the position of each coordinate pair from the central port
+    """
+    output = []
+    for index, coord in enumerate(relative_coords):
+        if index == 0:
+            # since we're assuming the origin of the central port is (0,0)
+            # we can make the first position that same as the first coordinate
+            output.append(coord)
+        else:
+            # otherwise, add the x-coord from the last coordinate
+            # with the x-coord of the current coordinate;
+            # the same is done for the y-coord
+            position = (output[-1][0] + coord[0], output[-1][1] + coord[1])
+            output.append(position)
+
+    return output
+
+
+# check work
+assert calculate_position([(1004, 518), (309, -991)]) == [(1004, 518), (1313, -473)]
+assert calculate_position([(-998, 952), (204, 266)]) == [(-998, 952), (-794, 1218)]
